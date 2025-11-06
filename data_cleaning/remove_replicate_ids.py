ba5d154af0fa416efd6967c2e7c6b6bcf695bc31
nohren <kuritake@protonmail.com>
2025-11-05 20:25:52 -0800

    data extraction process documented

--- data_cleaning/remove_replicate_ids.py
+++ data_cleaning/remove_replicate_ids.py
@@ -18,11 +18,13 @@ def main():
     except Exception as e:
         sys.exit(f"[error] failed to load BIOM table '{args.table}': {e}")
 
-    # Current sample IDs present in table
+    # Current sample IDs present in table and mapping to renamed IDs
     present = table.ids(axis="sample")
+    mapping = {c: ".".join(c.split(".")[:-1]) for c in present}
+    updated_table = table.update_ids(mapping, axis='sample', inplace=False)
 
-    with open(args.out, "w") as f:
-        f.write("\n".join(present))
+    with biom_open(args.out, "w") as f:
+        updated_table.to_hdf5(f, "remove_replicate_ids")
 
 
 if __name__ == "__main__":
